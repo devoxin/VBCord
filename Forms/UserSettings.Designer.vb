10a28b508af8f559cba9a42bb4b124b3a58cfd0f
Devoxin <lukel9914@gmail.com>
2017-08-14 21:03:46 +0100

    Add stuff to UserSettings, add todo, minor UI/code fixes etc

--- Forms/UserSettings.Designer.vb
+++ Forms/UserSettings.Designer.vb
@@ -23,21 +23,21 @@ Partial Class UserSettings
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.FormTemplate1 = New CrimsonCord.CrimCordTheme()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.CircularPictureBox1 = New CrimsonCord.CircularPictureBox()
         Me.ThemedButton1 = New CrimsonCord.ThemedButton()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.FormTemplate1.SuspendLayout()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CircularPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'FormTemplate1
         '
         Me.FormTemplate1.BackColor = System.Drawing.Color.FromArgb(CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer))
+        Me.FormTemplate1.Controls.Add(Me.CircularPictureBox1)
         Me.FormTemplate1.Controls.Add(Me.ThemedButton1)
-        Me.FormTemplate1.Controls.Add(Me.PictureBox1)
         Me.FormTemplate1.Controls.Add(Me.TextBox1)
         Me.FormTemplate1.Controls.Add(Me.Label2)
         Me.FormTemplate1.Controls.Add(Me.Label1)
@@ -50,58 +50,15 @@ Partial Class UserSettings
         Me.FormTemplate1.TabIndex = 0
         Me.FormTemplate1.TitleText = "User Settings"
         '
-        'ComboBox1
+        'CircularPictureBox1
         '
-        Me.ComboBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
-        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboBox1.ForeColor = System.Drawing.Color.White
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"Online", "Idle", "AFK", "Do Not Disturb", "Invisible"})
-        Me.ComboBox1.Location = New System.Drawing.Point(15, 132)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(159, 21)
-        Me.ComboBox1.TabIndex = 1
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Lato", 9.749999!)
-        Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(12, 113)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(95, 16)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Status Changer"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Lato", 9.749999!)
-        Me.Label2.ForeColor = System.Drawing.Color.White
-        Me.Label2.Location = New System.Drawing.Point(12, 39)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(118, 16)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Username Changer"
-        '
-        'TextBox1
-        '
-        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
-        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.TextBox1.Location = New System.Drawing.Point(15, 58)
-        Me.TextBox1.MaxLength = 32
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(159, 20)
-        Me.TextBox1.TabIndex = 4
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Location = New System.Drawing.Point(382, 39)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(90, 90)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.PictureBox1.TabIndex = 5
-        Me.PictureBox1.TabStop = False
+        Me.CircularPictureBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.CircularPictureBox1.Location = New System.Drawing.Point(382, 39)
+        Me.CircularPictureBox1.Name = "CircularPictureBox1"
+        Me.CircularPictureBox1.Size = New System.Drawing.Size(90, 90)
+        Me.CircularPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.CircularPictureBox1.TabIndex = 7
+        Me.CircularPictureBox1.TabStop = False
         '
         'ThemedButton1
         '
@@ -118,6 +75,51 @@ Partial Class UserSettings
         Me.ThemedButton1.Text = "Change..."
         Me.ThemedButton1.UseVisualStyleBackColor = False
         '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(15, 58)
+        Me.TextBox1.MaxLength = 32
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(159, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Lato", 9.749999!)
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(12, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(118, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Username Changer"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Lato", 9.749999!)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(12, 113)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Status Changer"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.ForeColor = System.Drawing.Color.White
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Online", "Idle", "AFK", "Do Not Disturb", "Invisible"})
+        Me.ComboBox1.Location = New System.Drawing.Point(15, 132)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(159, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
         'UserSettings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -132,7 +134,7 @@ Partial Class UserSettings
         Me.Text = "UserSettings"
         Me.FormTemplate1.ResumeLayout(False)
         Me.FormTemplate1.PerformLayout()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CircularPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -140,8 +142,8 @@ Partial Class UserSettings
     Friend WithEvents FormTemplate1 As CrimCordTheme
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents ThemedButton1 As ThemedButton
-    Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
+    Friend WithEvents CircularPictureBox1 As CircularPictureBox
 End Class
--- Forms/UserSettings.vb
+++ Forms/UserSettings.vb
@@ -1,4 +1,6 @@
-﻿Public Class UserSettings
+﻿Imports SelfUserEditor.Editor
+
+Public Class UserSettings
 
     Private Async Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
         If Not (ComboBox1.SelectedIndex + 1) = Main.Discord.CurrentUser.Status Then
@@ -8,7 +10,34 @@
 
     Private Sub UserSettings_Load(sender As Object, e As EventArgs) Handles Me.Load
         ComboBox1.SelectedIndex = Main.Discord.CurrentUser.Status
-        PictureBox1.ImageLocation = Main.Discord.CurrentUser.GetAvatarUrl()
+        CircularPictureBox1.ImageLocation = Main.Discord.CurrentUser.GetAvatarUrl()
     End Sub
 
+    Private Async Sub ThemedButton1_Click(sender As Object, e As EventArgs) Handles ThemedButton1.Click
+        Dim ofd As New OpenFileDialog With {
+            .Filter = "PNG|*.png",
+            .Title = "Select Avatar"
+            }
+
+        If ofd.ShowDialog = DialogResult.OK Then
+            Dim res As Boolean = Await EditUser(Main.Discord.CurrentUser, ofd.FileName, Nothing)
+            If (res) Then
+                CircularPictureBox1.ImageLocation = Main.Discord.CurrentUser.GetAvatarUrl()
+                Main.UserAvatar.ImageLocation = Main.Discord.CurrentUser.GetAvatarUrl()
+                Application.DoEvents()
+            End If
+        End If
+    End Sub
+
+    Private Async Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            e.Handled = True
+            Dim res As Boolean = Await EditUser(Main.Discord.CurrentUser, Nothing, TextBox1.Text)
+            If (res) Then
+                Main.Username.Text = Main.Discord.CurrentUser.Username
+                Main.UserDiscrim.Text = Main.Discord.CurrentUser.Discriminator
+                Application.DoEvents()
+            End If
+        End If
+    End Sub
 End Class
